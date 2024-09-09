ac3cc4c923a0619e33b3734c543f21bd8c45ce6d
mirzausama107 <usamamughal735@gmail.com>
2024-09-09 23:06:30 +0900

    Initial commit

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >= 0.8.0;
+import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory{
-    import "./ZombieFactory.sol";
+
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);  // only the onwer can feed the zombie
 
         Zombie storage myZombie = zombies[_zombieId];  // pointer for zombie in the blockchain
-        _targetDna = _targetDna % dnaModulus; // to make sure target dna is not longer than 16 digits
+        _targetDna = _targetDna % dnamodulus; // to make sure target dna is not longer than 16 digits
 
         uint newDna = (myZombie.dna + _targetDna) / 2;
         _createZombie("Usama", newDna);
